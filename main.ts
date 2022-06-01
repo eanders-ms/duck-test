fd5b390c37bf36464e0ceb6bd474e83dae7e651b
Eric Anderson <eanders@microsoft.com>
2022-06-01 15:06:00 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,4 +16,4 @@ let mySprite = sprites.create(img`
     . . . c c c c c c c c b b . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 100, 100)
